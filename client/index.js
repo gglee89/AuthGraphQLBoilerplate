aaf41e23909795484452cfa66b0ea0e3e4917d61
Gustavo Lee <gglee89@gmail.com>
2020-10-23 14:23:17 +0900

    feat: Integrate Client with GraphQL Apollo

--- client/index.js
+++ client/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Router
+import { Router, IndexRoute, Route, HashHistory } from 'react-router';
+
+// Apollo
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+// Components
+import App from './components/App'
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+const client = new ApolloClient({
+  networkInterface,
+  dataIdFromObject: o => o.id  
+})
+
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={HashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={} />
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,13 +1,17 @@
 const graphql = require('graphql');
 const { 
-  GraphQLObjectType,
-  GraphQLID
+  GraphQLObjectType
 } = graphql;
+
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    type: UserType,
+    resolve(parentValue, args, req) {
+      return req.user;
+    }
   }
 });
 
